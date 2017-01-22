b0fc597f9b16abde055a2312901484b4d191f328
Vlad Lashko <vlad.lashko@km-ware.com>
2017-01-23 00:01:56 +0200

    Can get still loading env as Observable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,31 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AppConfig {
   protected _config: Object = null;
 
+  protected resolutionPromise: Observable<Response> = null;
+
   constructor(protected http: Http) {
   }
 
-  public getConfig(key: string): string {
-    return this._config[key];
+  public getConfig(key: string): Observable<string> {
+    if (this._config !== null) {
+      return Observable.of(this._config[key]);
+    } else {
+      return this.resolutionPromise
+        .map(r => r.json())
+        .map(config => config[key]);
+    }
   }
 
   load() {
-    return this.http.get('env.json')
-      .subscribe(resp => {
-        this._config = resp.json();
-      });
+    const src = this.http.get('env.json');
+
+    this.resolutionPromise = src;
+    return src.subscribe(resp => this._config = resp.json());
   }
 }
