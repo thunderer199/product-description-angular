322809ce9f3c9346735ca2446e0e916f915cf58f
Vlad Lashko <vlad.lashko@km-ware.com>
2017-01-29 21:20:11 +0200

    Implement method for review creation

--- src/app/Review/review.service.ts
+++ src/app/Review/review.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
 import {AppConfig} from '../app.config';
-import {Http} from '@angular/http';
+import {Http, Headers} from '@angular/http';
 import {Observable} from 'rxjs';
 import {IReview} from './Review';
+import {AuthService} from '../User/auth.service';
 
 @Injectable()
 export class ReviewService {
 
-  constructor(protected config: AppConfig, protected http: Http) { }
+  constructor(protected config: AppConfig, protected http: Http, protected authService: AuthService) {
+  }
 
   getReviewForProduct(productId: number): Observable<IReview[]> {
     return this.config.getConfig('API_URL')
@@ -22,5 +24,15 @@ export class ReviewService {
       r.created_at = new Date(Date.parse(<string>r.created_at));
       return r;
     });
+  }
+
+  createReview(review: IReview, productId: number) {
+    // as we have auth token in one place, will use here with out interceptor for all app
+    const headers = new Headers();
+    headers.append('Authorization', `Token ${this.authService.token}`);
+
+    return this.config.getConfig('API_URL')
+      .flatMap(url => this.http.post(url + `/api/reviews/${productId}`, review, {headers}))
+      .map(r => r.json());
   }
 }
